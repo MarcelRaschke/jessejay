b065c9fe785161f5d0e377605252b6c9fc94c996
Marcel Raschke <42359664+MarcelRaschke@users.noreply.github.com>
2023-05-22 10:57:15 +0200

    Initialize revert-2-add-code-of-conduct

---
+++ gbook02.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Wayback Machine</title>
+    <script>
+    /*
+    @licstart  The following is the entire license notice for the JavaScript code in this page.
+
+    Copyright (C) 2020 Internet Archive
+
+    The JavaScript code in this page is free software: you can
+    redistribute it and/or modify it under the terms of the GNU Affero
+    General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option)
+    any later version.  The code is distributed WITHOUT ANY WARRANTY;
+    without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+
+    As additional permission under GNU AGPL version 3 section 7, you
+    may distribute non-source (e.g., minimized or compacted) forms of
+    that code without the copy of the GNU AGPL normally required by
+    section 4, provided you include this license notice and a URL
+    through which recipients can access the Corresponding Source.
+
+    @licend  The above is the entire license notice for the JavaScript code in this page.
+    */
+    </script>
+    <script type="text/javascript">
+      window.webComponentLoaderConfig = {
+      baseUrl: 'https://archive.org',
+      version: '29e56e87'
+      }
+    </script>
+    <script src="//archive.org/includes/jquery-1.10.2.min.js?v1.10.2" type="text/javascript"></script>
+    <script src="//archive.org/includes/analytics.js?v=04a93dbb" type="text/javascript"></script>
+    <link href="//archive.org/includes/build/css/archive.min.css?v=04a93dbb" rel="stylesheet" type="text/css">
+    <script type="text/javascript">if('archive_analytics' in window){var v=archive_analytics.values;v.path='/web';v.service='wb';v.server_name='wwwb-app220.us.archive.org';v.server_ms=5004;archive_analytics.send_pageview_on_load()}</script>
+    <link href="/_static/images/archive.ico" rel="shortcut icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="/_static/css/styles.css?v=X-Ndhul9" />
+    <script type="text/javascript" src="/_static/js/ui.js?v=NTAW0JFh" charset="utf-8"></script>
+    <base target="_top">
+    <link href="/_static/css/web.css" rel="stylesheet" type="text/css"/>
+    <!-- include component js and css -->
+    <link rel="stylesheet" type="text/css" href="/static/bower_components/wayback-search-js/dist/vendors~main.6a934aa18e24059078e7.chunk.css" />
+<link rel="stylesheet" type="text/css" href="/static/bower_components/wayback-search-js/dist/main.181a2460fd5cab984442.chunk.css" />
+<script src="/static/bower_components/wayback-search-js/dist/runtime.0a44659b8be5d61bab17.js"></script>
+<script src="/static/bower_components/wayback-search-js/dist/vendors~main.6a934aa18e24059078e7.chunk.js"></script>
+<script src="/static/bower_components/wayback-search-js/dist/main.181a2460fd5cab984442.chunk.js"></script>
+    <script src="https://archive.org/web/carouFredSel/jquery.carouFredSel-6.2.1-packed.js?v6.2.1" type="text/javascript"></script>
+    <style>
+      html, body { margin:0; padding:0 }
+      #faux-header {
+        height: 50px;
+        width: 100%;
+        margin:0;
+        background-color:black;
+      }
+      #react-wayback-search {
+        max-width: 1170px;
+        margin: 0 auto;
+        padding: 10px;
+      }
+    </style>
+  </head>
+  <body class="navia">
+    <!-- failed to fetch: <urlopen error timed out> -->
+    <script>update_aoheader();</script>
+    <div style="display: block; float: right; width: 28px; margin-top: 4px;">
+        <a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(window.location.href), '', 'height=400,width=600'); return false;"
+            class="social_button" title="Share on Facebook"><span class="iconochive-facebook" style="color:#3b5998;font-size:160%;"></span></a>
+        <a href="#" onclick="window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(window.location.href) + '&via=internetarchive', '', 'height=400,width=600'); return false;"
+            class="social_button" title="Share on Twitter"><span class="iconochive-twitter" style="color:#1dcaff;font-size:160%;"></span></a>
+    </div>
+    <script>
+      var config = {"api_url_anchor": "/__wb/search/anchor?q={{query}}", "api_url_calendar_captures": "/__wb/calendarcaptures?url={{url}}\u0026selected_year={{year}}", "api_url_host": "/__wb/search/host?q={{query}}", "api_url_metadata": "/__wb/search/metadata?q={{query}}", "api_url_sitemap": "/web/timemap/json?url={{url}}/\u0026fl=timestamp:4,original,urlkey\u0026matchType=prefix\u0026filter=statuscode:200\u0026filter=mimetype:text/html\u0026collapse=urlkey\u0026collapse=timestamp:4\u0026limit=100000", "api_url_sparkline": "/__wb/sparkline?output=json\u0026url={{url}}\u0026collection=web", "bundling": {"public_path": "/static/bower_components/wayback-search-js/dist"}, "diff": {"cdxServer": "/web/timemap", "diffgraphPrefix": "/web/diffgraph/", "limit": 3000, "maxSunburstLevelLength": "70", "snapshotsPrefix": "/web/", "sparklineURL": "/__wb/sparkline", "urlPrefix": "/web/diff/", "waybackDiscoverDiff": "https://gext-api.archive.org/services/simhash", "webMonitoringProcessingURL": "https://gext-api.archive.org/services/diff"}, "features": {"beta_promo": false, "changes_calendar": true, "landing": "carousel", "landing_layout_with_trend": false, "search": "v1"}, "num_of_explored_web_pages": "766 billion", "search_v2": {"search_collections": {"defaultIntro": "Welcome to the Wayback Machine collection search service. This service is based on indexes of specific data from selected Collections.\n", "defaultMore": "Collections are groupings of Web archives related to a specific archiving method or source of URLs. For example the \u003ca href=\"https://archive.org/details/mediacloud\"\u003emediacloud\u003c/a\u003e Collection contains URLs sourced from \u003ca href=\"https://mediacloud.org\"\u003emediacloud.org\u003c/a\u003e.\u003cbr /\u003e\u003cbr /\u003e The specific Web page data we are indexing includes: anchor text, all URL parts (file names, hosts, domains), MIME Types, HTTP Status codes \u0026 Web Page Titles. In this way you can search for PDF files, from .mil websites that have the word \u201cnuclear\u201d in their file name.\n", "options": {"EndOfTerm2008WebCrawls": {"intro": "Search #count web pages using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, language, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "End Of Term (US Gov) 2008", "url": "https://archive.org/details/cdlweb"}, "EndOfTerm2012WebCrawls": {"intro": "Search #count web pages using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, language, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "index_mapping": {"text": "search2:eot2012-search-text-202210"}, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "End Of Term (US Gov) 2012", "url": "https://archive.org/details/EndOfTerm2012WebCrawls"}, "EndOfTerm2016WebCrawls": {"intro": "Search #count web pages using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, language, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "End Of Term (US Gov) 2016", "url": "https://archive.org/details/EndOfTerm2016WebCrawls"}, "EndOfTerm2020WebCrawls": {"intro": "Search #count web pages using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, language, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "End Of Term (US Gov) 2020", "url": "https://archive.org/details/EndOfTerm2020WebCrawls"}, "badoo.com": {"intro": "Search #count badoo.com articles using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "badoo.com"}, "gov": {"intro": "Search #count .gov web pages using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, HTTP Status codes, Web Page Titles and all the text from the first page of PDF documents. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": ".gov web pages"}, "gov-pdf": {"intro": "Search #count .gov PDF files using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, HTTP Status codes, Web Page Titles and all the text from the first page of PDF documents. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "pdf", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "pdf", "items": ["application/pdf"], "title": "PDFs", "type": "application/pdf"}]}, "title": ".gov PDFs"}, "hk.appledaily.com": {"intro": "Search #count hk.appledaily.com articles using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "hk.appledaily.com"}, "mediacloud": {"intro": "Search #count news articles using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, language, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "Media Cloud", "url": "https://archive.org/details/mediacloud"}, "pdf": {"intro": "Search #count PDF files using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, language, HTTP Status codes, Web Page Titles and all the text from the first page of PDF documents. Index last updated on 2021/10/12. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": "", "search_types": {"default": "pdf", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "pdf", "items": ["application/pdf"], "title": "PDFs", "type": "application/pdf"}]}, "title": "PDFs"}, "poetry.com": {"intro": "Search #count poetry.com articles using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "poetry.com"}, "presentations": {"intro": "Search #count Presentations (MS Powerpoint, Apple Keynote, OpenOffice Impress) using an index built with the full text, anchor text, all URL parts (file names, hosts, domains), MIME Types, language and HTTP status codes. Index last updated on 12/10/2021. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "application", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "application", "title": "Presentations", "type": "application"}]}, "title": "Presentations"}, "russian-independent-media": {"intro": "Search #count Russian independent media news articles using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages"}]}, "title": "Russian Independent Media"}, "scmp.com": {"intro": "Search #count scmp.com news articles using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "South China Morning Post (scmp.com)"}, "thestandnews.com": {"intro": "Search #count thestandnews.com articles using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "thestandnews.com"}, "zaman.com.tr": {"intro": "Search #count zaman.com.tr news articles using an index built from anchor text, all URL parts (file names, hosts, domains), MIME Types, HTTP Status codes and the full text of the articles. \u003cbr /\u003e\u003cbr /\u003e Please send your questions, suggestions, bug reports, etc. to \u003ca href=\"mailto:info@archive.org\"\u003einfo@archive.org\u003c/a\u003e.\n", "more": " ", "search_types": {"default": "text", "horizontal_line_room": 1, "options": [{"icon": "iconochive-texts", "id": "text", "title": "Webpages", "type": "text"}]}, "title": "zaman.com.tr"}}}}, "url_playback": "/web/{{date}}/{{url}}", "url_playback_screenshot": "/web/", "url_save_page_now": "/save/{{query}}", "url_search_page": "/web/*/{{url}}*", "url_thumb": "https://web.archive.org/thumb/{{item}}?generate=1"};
+      $(document).ready(function(){
+	var e = $('#react-wayback-search'), c = e.attr('component');
+	ReactDOM.render(
+	  React.createElement(window[c], { config: config }),
+	  e.get(0)
+	);
+      });
+    </script>
+    <div id="react-wayback-search" component="WaybackSearchComponent"></div>
+    <noscript>
+      <div class="no-script-message">
+        The Wayback Machine requires your browser to support JavaScript, please email <a href="mailto:info@archive.org">info@archive.org</a><br/>if you have any questions about this.
+      </div>
+    </noscript>
+    <footer>
+      <div id="footerHome">
+        <p>
+          The Wayback Machine is an initiative of the
+          <a href="//archive.org/">Internet Archive</a>,
+          a 501(c)(3) non-profit, building a digital library of
+          Internet sites and other cultural artifacts in digital form.
+          <br/>Other <a href="//archive.org/projects/">projects</a> include
+          <a href="https://openlibrary.org/">Open Library</a> &amp;
+          <a href="https://archive-it.org">archive-it.org</a>.
+        </p>
+        <p>
+          Your use of the Wayback Machine is subject to the Internet Archive's
+          <a href="//archive.org/about/terms.php">Terms of Use</a>.
+        </p>
+      </div>
+    </footer>
+  </body>
+</html>
